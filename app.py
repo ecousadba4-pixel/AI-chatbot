e6a5e4efa9939b1922eda8ee6fece0e731fe509e
ecousadba4-pixel <ecousadba4@gmail.com>
2025-10-22 16:10:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,6 @@ def health_check():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
+
 
